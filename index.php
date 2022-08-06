f3f19deacc314e9bf6537a6ceaa561ced7e87ebb
avinashyeccalluri <yeccaluriavinash@gmail.com>
2022-08-06 13:40:46 +0530

    "Test client change as per STEVE"

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ include('picConfig.php');
     <meta name="description" content="ARTEG Architects & Associates is one of the top builders in vellore & kanchipuram and we will build your homes, apartments, offices, hospitals anywhere which suits your budget and requirements. We are expert in building modern designs in vellore & kanchipuram.">
     <meta name="keywords" content="Vellore Builders, Builders in Vellore, Construction companies in Vellore, Builders, Construction company in Vellore, Best builders in Vellore, Top construction companies in Vellore, Building & Construction services in vellore, Building & Construction companies in vellore, Low cost Builders in Vellore, Housing and constructions in vellore, Reputed builders in Vellore, leading builders in vellore, Modern design Builders in vellore, Builders and Flat Promoters in Vellore, Building Contractors in vellore, Civil Contractors in vellore, Residential Building Contractors in vellore, Commercial Building Contractors in vellore, Construction Contractors in Vellore, House Builders in vellore, Top Builders in Vellore, Best construction company in Vellore, Top 10 construction companies in Vellore, Low cost construction company in Vellore, Modern design Constructors in vellore, Constructions in Vellore, Construction Services in vellore, Construction Services company in vellore, Interior Design in Vellore, Vellore Interiors, vellore interiors design, Modular Kitchen Interiors, Affordable Interior Design in Vellore, Good interior designers in vellore, Best interior Designers in Vellore, top interior designers in vellore, modular kitchen in vellore, Stylish interior designer in vellore, Architectural & Interior designing, Interior Decorators in Vellore, interior decorators in vellore, interiors in vellore, best interior decorators in vellore, home interior decorators in vellore, interior decors in vellore, bedroom interiors, decorators in vellore, home interiors in vellore, home interior designers in vellore, interior designers for home in vellore, interior works for home in vellore, home decor interior designers in vellore, interior design services in vellore, Modern interior designer in vellore, Residential interior Decorators in vellore, Residential interiors vellore, Architecture in Vellore, Architects in vellore, List of Architects in Vellore, Architecture Firms in Vellore, Top Architects in Vellore, Best Architects in Vellore, Architectural Designing Services in Vellore, Architectural Drafting Services in Vellore, Architectural Services in Vellore, Architecture Services in Vellore, Vellore Architects, Architect Consultant in Vellore, Architectural Design in vellore, Architecture Design in vellore, interior architects in vellore, architecture vellore, Low cost Architectural Designing Services in Vellore, Kanchipuram Builders, Builders in Kanchipuram, Construction companies in Kanchipuram, Builders, Construction company in Kanchipuram, Best builders in Kanchipuram, Top construction companies in Kanchipuram, Low cost Builders in Kanchipuram, Housing and constructions in Kanchipuram, Reputed builders in Kanchipuram, leading builders in kanchipuram, Modern design Builders in Kanchipuram, Builders and Flat Promoters in Kanchipuram, Building & Construction companies in Kanchipuram, Building Contractors in Kanchipuram, Civil Contractors in Kanchipuram, Residential Building Contractors in Kanchipuram, Commercial Building Contractors in kanchipuram, Building & Construction services in Kanchipuram, Construction Contractors in Kanchipuram, House Builders in Kanchipuram, Top Builders in Kanchipuram, Best construction company in Kanchipuram, Top 10 construction companies in Kanchipuram, Low cost construction company in Kanchipuram, Modern design Constructors in Kanchipuram, Constructions in Kanchipuram, Construction Services in Kanchipuram, Construction Services company in Kanchipuram, Interior Design in Kanchipuram, Kanchipuram Interiors, Kanchipuram interiors design, Modular Kitchen Interiors, Affordable Interior Design in Kanchipuram, Good interior designers in Kanchipuram, Best interior Designers in Kanchipuram, top interior designers in Kanchipuram, modular kitchen in Kanchipuram, Stylish interior designer in Kanchipuram, Architectural & Interior designing, Interior Decorators in Kanchipuram, interior decorators in Kanchipuram, interiors in Kanchipuram, best interior decorators in Kanchipuram, home interior decorators in Kanchipuram, interior decors in Kanchipuram, bedroom interiors, decorators in Kanchipuram, home interiors in Kanchipuram, home interior designers in Kanchipuram, interior designers for home in Kanchipuram, interior works for home in Kanchipuram, home decor interior designers in Kanchipuram, interior design services in Kanchipuram, Modern interior designer in Kanchipuram, Residential interior Decorators in Kanchipuram, Residential interiors Kanchipuram, Architecture in Kanchipuram, Architects in Kanchipuram, List of Architects in Kanchipuram, Architecture Firms in Kanchipuram, Top Architects in Kanchipuram, Best Architects in Kanchipuram, Architectural Designing Services in Kanchipuram, Architectural Drafting Services in Kanchipuram, Architectural Services in Kanchipuram, Architecture Services in Kanchipuram, Kanchipuram Architects, Architect Consultant in Kanchipuram, Architectural Design in Kanchipuram, Architecture Design in Kanchipuram, interior architects in Kanchipuram, architecture Kanchipuram, Low cost Architectural Designing Services in Kanchipuram, Arteg Architects & Associates">
     <meta name="author" content="">
-    
+
     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
 
@@ -73,8 +73,6 @@ include('picConfig.php');
             border: solid 1px #c99304;
 
         }
-
-
     </style>
 </head>
 
@@ -229,64 +227,6 @@ include('picConfig.php');
             <!-- section close -->
 
 
-            <!-- section begin -->
-            <section id="section-steps" class="text-light">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 col-md-offset-3 text-center wow fadeInUp">
-                            <h1>Our Process</h1>
-                            <div class="separator"><span><i class="fa fa-circle"></i></span></div>
-                            <div class="spacer-single"></div>
-                        </div>
-
-                        <div class="col-md-12">
-                            <div class="de_tab tab_steps">
-                                <ul class="de_nav">
-                                    <li class="active wow fadeIn" data-wow-delay="0s"><span>Meet &amp; Agree</span>
-                                        <div class="v-border"></div>
-                                    </li>
-                                    <li class="wow fadeIn" data-wow-delay=".4s"><span>Idea &amp; Concept</span>
-                                        <div class="v-border"></div>
-                                    </li>
-                                    <li class="wow fadeIn" data-wow-delay=".8s"><span>Design &amp; Create</span>
-                                        <div class="v-border"></div>
-                                    </li>
-                                    <li class="wow fadeIn" data-wow-delay="1.2s"><span>Build &amp; Install</span>
-                                        <div class="v-border"></div>
-                                    </li>
-                                </ul>
-
-                                <div class="de_tab_content">
-
-                                    <div id="tab1">
-                                        We collaborate with our clients in every level during the design process to serve their goals. Our clients vision guided with our experiences comes in an outstanding result. ARTEG is dedicated to change the life of people by its design. Our deeply rooted concern is answering the society needs through our buildings while maintaining a balanced sustainable use of resources providing the opportunity for a better Life with economic growth and development.
-                                    </div>
-
-                                    <div id="tab2">
-                                        We consciously devote consistent energy and enthusiasm to each
-                                        and every project by seamlessly collaborating Architecture, Interiors,
-                                        Product and Furniture designs. We create highly unique designs
-                                        both functionally and aesthetically by exploring the latest in technological with bespoke craftsmanship.
-
-                                    </div>
-
-                                    <div id="tab3">
-                                        We design spaces in such a way that each square meter speaks for itself. The architecture imparts a vibrant atmosphere that sings to the tunes of nature around. We mix various styles ensuring that you get custom designed interiors that speak your soul.<br>
-                                        we offer our clients stupendous projects that manifests a ‘macro to micro’ level attention from big ideas to even smallest details giving coherence to the entire built site. We offer a single point of contact for all project related services.
-                                    </div>
-
-                                    <div id="tab4">
-                                        Besides consulting works, Arteg, the top architects and consultants in Chennai, offers a wide range of turnkey interior contracting works too. We stress on creating bold projects that awes the eyes of the beholder and inspires others to take bolder steps.<br>
-                                        We stay ahead of the trends and technology incorporating the latest trends in the most fashionable manner. Our interiors speak about dwelling that creates desires and impressions!
-                                    </div>
-
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
             <!-- section close -->
 
 
@@ -334,7 +274,7 @@ include('picConfig.php');
 
                     ?>
 
-                            <div class="item grid-item <?php echo $class.' '.$key; ?> ">
+                            <div class="item grid-item <?php echo $class . ' ' . $key; ?> ">
                                 <div class="picframe">
                                     <a class="simple-ajax-popup-align-top" href="project_details.php?id=<?php echo $value_1['id']; ?>">
                                         <span class="overlay">
@@ -400,6 +340,64 @@ include('picConfig.php');
             <!-- logo carousel section close -->
 
 
+            <!-- section begin -->
+            <section id="section-steps" class="text-light">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3 text-center wow fadeInUp">
+                            <h1>Our Process</h1>
+                            <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                            <div class="spacer-single"></div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="de_tab tab_steps">
+                                <ul class="de_nav">
+                                    <li class="active wow fadeIn" data-wow-delay="0s"><span>Meet &amp; Agree</span>
+                                        <div class="v-border"></div>
+                                    </li>
+                                    <li class="wow fadeIn" data-wow-delay=".4s"><span>Idea &amp; Concept</span>
+                                        <div class="v-border"></div>
+                                    </li>
+                                    <li class="wow fadeIn" data-wow-delay=".8s"><span>Design &amp; Create</span>
+                                        <div class="v-border"></div>
+                                    </li>
+                                    <li class="wow fadeIn" data-wow-delay="1.2s"><span>Build &amp; Install</span>
+                                        <div class="v-border"></div>
+                                    </li>
+                                </ul>
+
+                                <div class="de_tab_content">
+
+                                    <div id="tab1">
+                                        We collaborate with our clients in every level during the design process to serve their goals. Our clients vision guided with our experiences comes in an outstanding result. ARTEG is dedicated to change the life of people by its design. Our deeply rooted concern is answering the society needs through our buildings while maintaining a balanced sustainable use of resources providing the opportunity for a better Life with economic growth and development.
+                                    </div>
+
+                                    <div id="tab2">
+                                        We consciously devote consistent energy and enthusiasm to each
+                                        and every project by seamlessly collaborating Architecture, Interiors,
+                                        Product and Furniture designs. We create highly unique designs
+                                        both functionally and aesthetically by exploring the latest in technological with bespoke craftsmanship.
+
+                                    </div>
+
+                                    <div id="tab3">
+                                        We design spaces in such a way that each square meter speaks for itself. The architecture imparts a vibrant atmosphere that sings to the tunes of nature around. We mix various styles ensuring that you get custom designed interiors that speak your soul.<br>
+                                        we offer our clients stupendous projects that manifests a ‘macro to micro’ level attention from big ideas to even smallest details giving coherence to the entire built site. We offer a single point of contact for all project related services.
+                                    </div>
+
+                                    <div id="tab4">
+                                        Besides consulting works, Arteg, the top architects and consultants in Chennai, offers a wide range of turnkey interior contracting works too. We stress on creating bold projects that awes the eyes of the beholder and inspires others to take bolder steps.<br>
+                                        We stay ahead of the trends and technology incorporating the latest trends in the most fashionable manner. Our interiors speak about dwelling that creates desires and impressions!
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <!-- section begin -->
             <?php
             include('customer_says.php');
