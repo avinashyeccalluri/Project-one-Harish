24a5ebd40d21cba7293416f6aa16ad59d95ca6b8
avinashyeccalluri <yeccaluriavinash@gmail.com>
2022-08-06 12:43:10 +0530

    t-2
    Signed-off-by: avinashyeccalluri <yeccaluriavinash@gmail.com>

--- config.php
+++ config.php
@@ -3,6 +3,4 @@ $servername = "localhost";
 $username = "arteg_db";
 $password = "arteg123!@#";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
-mysqli_select_db($conn,"arteg_db");
 ?>
